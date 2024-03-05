48e8d3cf4d223e8a4fb742dd7685e1bd9893eb32
sagarbisht509 <sagarbisht509@gmail.com>
2024-03-05 22:38:48 +0530

    Initial commit

---
+++ app/src/main/java/com/sagar/picture_in_picture/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.sagar.picture_in_picture
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sagar.picture_in_picture.ui.theme.Picture_in_pictureTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Picture_in_pictureTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Picture_in_pictureTheme {
+        Greeting("Android")
+    }
+}
