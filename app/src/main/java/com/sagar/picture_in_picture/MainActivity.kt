b2ea4be903ff5a8e06ee7f11917f8acf22c06953
sagarbisht509 <sagarbisht509@gmail.com>
2024-03-05 22:43:22 +0530

    Implemented PIP

--- app/src/main/java/com/sagar/picture_in_picture/MainActivity.kt
+++ app/src/main/java/com/sagar/picture_in_picture/MainActivity.kt
@@ -1,46 +1,93 @@
 package com.sagar.picture_in_picture
 
+import android.app.PictureInPictureParams
+import android.graphics.Rect
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.util.Rational
+import android.widget.VideoView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.layout.boundsInWindow
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.viewinterop.AndroidView
 import com.sagar.picture_in_picture.ui.theme.Picture_in_pictureTheme
 
 class MainActivity : ComponentActivity() {
+
+    private var videoViewBounds = Rect()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Picture_in_pictureTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MainContent(
+                    packageName = packageName,
+                    temp = { videoViewBounds = it }
+                )
             }
         }
     }
+
+
+    /*
+     gets called when the user leaves the current activity without explicitly finishing it,
+     such as by pressing the Home button, receiving a phone call, or launching another activity.
+     */
+    override fun onUserLeaveHint() {
+        super.onUserLeaveHint()
+
+        updatedPipParams()?.let { params ->
+            if (checkSupport()) {
+                enterPictureInPictureMode(params)
+            }
+        }
+
+    }
+
+    private fun updatedPipParams(): PictureInPictureParams? {
+        return if (checkSupport()) {
+            PictureInPictureParams.Builder()
+                // provides smoother animation for transitioning to video content in PiP mode
+                .setSourceRectHint(videoViewBounds)
+                .setAspectRatio(Rational(16, 9))
+                .build()
+        } else null
+    }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun MainContent(
+    packageName: String,
+    temp: (Rect) -> Unit
+) {
+    val videoUri = Uri.parse("android.resource://${packageName}/${R.raw.video_name}")
+    AndroidView(
+        factory = { context ->
+            VideoView(context, null).apply {
+                setVideoURI(videoUri)
+                start()
+            }
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .onGloballyPositioned {
+                val boundsInWindow = it.boundsInWindow()
+                temp(
+                    Rect(
+                        boundsInWindow.left.toInt(),
+                        boundsInWindow.top.toInt(),
+                        boundsInWindow.right.toInt(),
+                        boundsInWindow.bottom.toInt()
+                    )
+                )
+            }
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Picture_in_pictureTheme {
-        Greeting("Android")
-    }
-}
+private fun checkSupport() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
